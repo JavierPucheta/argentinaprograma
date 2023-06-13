9a7dd24bb570cec6480b0b132bc56a497c963407
JavierPucheta <patriciacaceres_3@hotmail.com>
2023-06-13 20:52:36 -0300

    Ejercicio 1b

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,11 @@
+const readlineSync = require('readline-sync');
+const num = readlineSync.question('Ingrese un numero: ');
+if (num > 0) {
+    console.log("El numero es positivo");
+}
+else if (num == 0) {
+    console.log("El numero es cero");
+}
+else if (num < 0) {
+    console.log("El numero es negativo");
+}
